079bc8504fca7357b3f5185ae54f1c84ec63cbee
SavindraSinghShekhawat <shekhawatsavindra@gmail.com>
2023-12-31 18:48:28 +0530

    Initial commit

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import find_packages, setup
 setup(
     name='statshub',
-    packages=find_packages(include=['statshub']),
-    version='0.0.4',
+    packages=find_packages(),
+    version='0.0.5',
     description='Python Library to perform various Mathematical and Statical Operations.',
     author='Savi',
     license='MIT',
